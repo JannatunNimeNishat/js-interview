f969841d45a8a87a5c981d7a04594666994ff01e
JannatunNimeNishat <nishatnime100@gmail.com>
2023-07-22 23:22:14 +0600

    vowelAndConsonantCount

---
+++ JS/clouser/clouser.js
@@ -0,0 +1,17 @@
+
+const sum = (a,b) =>{
+    console.log(a+b);
+}
+sum(1,2)
+
+
+
+const flag = 5;
+
+const substract = (a) =>{
+    console.log(flag - a);
+}
+
+substract(1)
+
+
---
+++ problem_solving/vowelAndConsonants.js
@@ -0,0 +1,25 @@
+//Q. Write a code on how to calculate the number of vowels and consonants in a string?(Use Javascript only)
+
+function countVowelAndConsonant(myName){
+    let vowelCount = 0;
+    let consonant = 0;
+    const vowel = "aeiouAEIOU"
+    for(let i=0; i<myName.length;i++){
+        const char = myName[i]
+        if(vowel.indexOf(char) !== -1){
+            vowelCount ++
+        }
+        else{
+            consonant ++
+        }
+    
+    }
+    return {vowelCount, consonant}
+}
+
+
+const {vowelCount, consonant} = countVowelAndConsonant('Md. Jannatun Nime Nishat')
+
+console.log(`Number of vowel: ${vowelCount}, Number of consonant: ${consonant}`);
+
+
