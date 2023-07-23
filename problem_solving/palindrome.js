8e68d3d0f16e64dc6f2dcc1d8c1efcc23ec3d09f
JannatunNimeNishat <nishatnime100@gmail.com>
2023-07-23 12:50:29 +0600

    palindrome

---
+++ problem_solving/palindrome.js
@@ -0,0 +1,15 @@
+// Q. Write a code to calculate if the string is palindrome ? (Use javascript only)
+
+function checkPalindrome(myString){
+    
+    if(myString == myString.split('').reverse().join('')){
+        return `${myString} is  palindrome`;
+    }
+    else{
+        return `${myString} is not palindrome`;
+    }
+
+}
+
+console.log(checkPalindrome('WOW'));
+
